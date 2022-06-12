865ee48d0eb23f936c314a65f920ceb4809054c6
Angel G Maxwell <agmaxwellh@gmail.com>
2022-06-12 19:43:49 -0400

    agregar logo a formulario de datos filament

--- resources/views/temporary/create.blade.php
+++ resources/views/temporary/create.blade.php
@@ -1,6 +1,11 @@
 <x-guest-layout>
-    <div class="flex h-screen justify-center mt-10">
-        @livewire('tramites.crear-tramite')
+    <div class="flex items-center justify-center mt-8">
+        <img class="w-16 h-auto" src="/images/escudo.png" alt="">
+        <img class="w-auto h-12 ml-4" src="/images/letras.png" alt="">
+    </div>
+    <div class="flex justify-center mt-8">
+        <div class="md:px-8 md:py-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-10">
+            @livewire('tramites.crear-tramite')
+        </div>
     </div>
 </x-guest-layout>
-
