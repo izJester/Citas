b04f9c60574530928c04a65bbf5ad3d2b4552bac
Angel G Maxwell <agmaxwellh@gmail.com>
2022-06-25 14:00:15 -0400

    Mejora visual simple de formulario de estatus

--- resources/views/temporary/estatus.blade.php
+++ resources/views/temporary/estatus.blade.php
@@ -1,10 +1,14 @@
 <x-guest-layout>
-       <div class="flex space-x-2 my-12">
-                <div class="w-full p-6">
-                    <div class="p-6 shadow bg-white">
-                       @livewire('estatus')
-                    </div>
-                </div>
-            </div>
+    <div class="flex items-center justify-center mt-8">
+        <img class="w-16 h-auto" src="/images/escudo.png" alt="">
+        <img class="w-auto h-12 ml-4" src="/images/letras.png" alt="">
+    </div>
+    <div class="flex justify-center mt-8 px-4">
+        <div class="w-full md:w-1/2 md:px-8 px-4 py-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-10">
+            <p class="mb-8">
+                Utilice este formulario para conocer el estatus de su solicitud de documentos ante la Secretaría General de la Universidad Nacional Experimental de la Fuerza Armada Nacional Bolivariana
+            </p>
+            @livewire('estatus')
+        </div>
+    </div>
 </x-guest-layout>
-
